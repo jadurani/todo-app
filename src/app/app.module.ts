558c94a20215060b8cbbc34182dc2b4cebf82852
Jadurani Davalos <jadurani.davalos@gmail.com>
2017-11-15 17:12:20 +0800

    working input output on todo-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo-input/todo-input.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+// import { TodoService } from '../todo.service';
+
+@Component({
+  selector: 'todo-input',
+  templateUrl: './todo-input.component.html',
+  styleUrls: ['./todo-input.component.css']
+})
+export class TodoInputComponent implements OnInit {
+
+  // constructor(public todoService:TodoService) {}
+
+  onClick(value) {
+    console.log(value);
+    // this.todoService.todos.push(value);
+    // console.log(this.todoService.todos);
+  }
+
+  ngOnInit() {}
+}
